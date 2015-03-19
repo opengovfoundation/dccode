6fb6a9bccde28d750987bb270d352a6e8445f668
Bill Hunt <bill@krues8dr.com>
2015-03-19 10:55:39 -0400

    Adding new UserVoice account.

--- htdocs/themes/DCDecoded2013/static/js/vendor/uservoice.js
+++ htdocs/themes/DCDecoded2013/static/js/vendor/uservoice.js
@@ -1,5 +1,5 @@
 // UserVoice JavaScript SDK (only needed once on a page)
-(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/2jyFuipxoHLOCGmgrsClZw.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()
+(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/9oqW3FHua8BEDQfvSuLmjg.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()
 
 // A tab to launch the Classic Widget
 UserVoice = window.UserVoice || [];
