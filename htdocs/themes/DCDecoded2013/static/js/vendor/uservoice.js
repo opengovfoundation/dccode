3dbf19f4a45db789516151ec0deafa8ddb5e50ae
Bill Hunt <bill@krues8dr.com>
2015-03-19 11:34:53 -0400

    Updating UserVoice widget.

--- htdocs/themes/DCDecoded2013/static/js/vendor/uservoice.js
+++ htdocs/themes/DCDecoded2013/static/js/vendor/uservoice.js
@@ -1,16 +1,14 @@
-// UserVoice JavaScript SDK (only needed once on a page)
 (function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/9oqW3FHua8BEDQfvSuLmjg.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()
 
-// A tab to launch the Classic Widget
 UserVoice = window.UserVoice || [];
 UserVoice.push(['showTab', 'classic_widget', {
   mode: 'full',
-  primary_color: '#cc6d00',
-  link_color: '#007dbf',
+  primary_color: '#137da1',
+  link_color: '#137da1',
   default_mode: 'support',
-  forum_id: 225698,
+  forum_id: 286788,
   tab_label: 'Feedback & Support',
-  tab_color: '#69869d',
+  tab_color: '#cc6d00',
   tab_position: 'middle-left',
   tab_inverted: false
 }]);
